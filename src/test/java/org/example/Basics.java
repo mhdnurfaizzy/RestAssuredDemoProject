3d9151c67787d05200b6dc3b46613a1d8b048734
JIH <mhdnurfaizzy@yahoo.com>
2024-06-02 14:21:22 +0700

    add test to update place id

--- src/test/java/org/example/Basics.java
+++ src/test/java/org/example/Basics.java
@@ -14,12 +14,13 @@ public class Basics {
         //When - Submit the API - resource, http method
         //Then- Validate the response
 
+        //Add Place testing
+
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         String response = given().log().all().queryParam("key", "qaclick123").header("Content-type",  "application/json")
                 .body(payload.addPlace())
                 .when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
-                .header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 
         System.out.println("===RESPONSE BODY===");
         System.out.println(response);
@@ -27,6 +28,18 @@ public class Basics {
         String placeID = js.getString("place_id");
 
         System.out.println(placeID);
+
+        //Update Place Testing
+        given().log().all().queryParam("key", "qaclick123").header("Content-type",  "application/json")
+                .body("{\n" +
+                        "\"place_id\":\""+placeID+"\",\n" +
+                        "\"address\":\"70 winter walk, USA\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}")
+                .when().put("maps/api/place/update/json")
+                .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        //Get Place Testing - verify place updated
 
     }
 }
