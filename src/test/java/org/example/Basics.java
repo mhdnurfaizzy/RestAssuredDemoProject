550f3f608dab5eafd6d2bc1eee53f30b61f4169e
JIH <mhdnurfaizzy@yahoo.com>
2024-06-02 14:08:09 +0700

    parsing json response boy using JsonPath class

--- src/test/java/org/example/Basics.java
+++ src/test/java/org/example/Basics.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -14,11 +15,18 @@ public class Basics {
         //Then- Validate the response
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all().queryParam("key", "qaclick123").header("Content-type",  "application/json")
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-type",  "application/json")
                 .body(payload.addPlace())
                 .when().post("maps/api/place/add/json")
                 .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
-                .header("server", "Apache/2.4.52 (Ubuntu)");
+                .header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+
+        System.out.println("===RESPONSE BODY===");
+        System.out.println(response);
+        JsonPath js = new JsonPath(response);
+        String placeID = js.getString("place_id");
+
+        System.out.println(placeID);
 
     }
 }
