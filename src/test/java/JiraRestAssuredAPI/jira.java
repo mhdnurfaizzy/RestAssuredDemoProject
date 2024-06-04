13ee558ee7e807e542d05ec0265de2a777b6c016
JIH <mhdnurfaizzy@yahoo.com>
2024-06-04 11:18:20 +0700

    add test Login Scenario - Using Cookies

--- src/test/java/JiraRestAssuredAPI/jira.java
+++ src/test/java/JiraRestAssuredAPI/jira.java
@@ -1,6 +1,7 @@
 package JiraRestAssuredAPI;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
 
 import static io.restassured.RestAssured.given;
 
@@ -11,15 +12,26 @@ public class jira {
 
         RestAssured.baseURI="http://localhost:8080";
 
-        //Add Comment
-        given().pathParam("key", "JIR").log().all().header("Content-Type", "application/json")
+        //Login Scenario - Using Cookies
+        SessionFilter session = new SessionFilter();
+        String response = given().header("Content-Type", "application/json")
+                        .body("{ \n" +
+                                "    \"username\": \"your-username\", \n" +
+                                "    \"password\": \"your-password\" \n" +
+                                "}").log().all().filter(session)
+                        .when().post("rest/auth/1/session")
+                        .then().log().all().extract().response().asString();
+
+        //Add Comment to issue
+        given().pathParam("key", "JIR-1").log().all().header("Content-Type", "application/json")
                 .body("{\n" +
                         "    \"body\": \"Add comment to issue using restassured API\",\n" +
                         "    \"visibility\": {\n" +
                         "        \"type\": \"role\",\n" +
                         "        \"value\": \"Administrators\"\n" +
                         "    }\n" +
-                        "}")
-                .post("/rest/api/2/issue/{key}/comment");
+                        "}").filter(session)
+                .when().post("/rest/api/2/issue/{key}/comment")
+                .then().log().all().assertThat().statusCode(201);
     }
 }
