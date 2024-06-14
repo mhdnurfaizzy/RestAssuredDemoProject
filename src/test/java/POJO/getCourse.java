c95968c6c7c2b6a1561552754520b298222fc00d
JIH <mhdnurfaizzy@yahoo.com>
2024-06-14 21:30:15 +0700

    pojo file

---
+++ src/test/java/POJO/getCourse.java
@@ -0,0 +1,62 @@
+package POJO;
+
+public class getCourse {
+
+
+    private String url;
+    private String services;
+    private String expertice;
+    private String courses;
+    private String instructor;
+    private String linkedin;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public String getExpertice() {
+        return expertice;
+    }
+
+    public void setExpertice(String expertice) {
+        this.expertice = expertice;
+    }
+
+    public String getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String courses) {
+        this.courses = courses;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+
+}
