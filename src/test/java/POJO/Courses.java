0a71daf7b028dd0ea4703c7adb1b9dc26ee820c3
JIH <mhdnurfaizzy@yahoo.com>
2024-06-15 14:26:12 +0700

    create pogo webAutomation, api, and mobile to handle nested json response

--- src/test/java/POJO/Courses.java
+++ src/test/java/POJO/Courses.java
@@ -1,33 +1,37 @@
 package POJO;
 
 public class Courses {
-    private String webAutomation;
-    private String api;
-    private String mobile;
 
-    public String getWebAutomation() {
+    private webAutomation webAutomation;
+    private api api;
+    private mobile mobile;
+
+
+    public POJO.webAutomation getWebAutomation() {
         return webAutomation;
     }
 
-    public void setWebAutomation(String webAutomation) {
+    public void setWebAutomation(POJO.webAutomation webAutomation) {
         this.webAutomation = webAutomation;
     }
 
-    public String getApi() {
+    public POJO.api getApi() {
         return api;
     }
 
-    public void setApi(String api) {
+    public void setApi(POJO.api api) {
         this.api = api;
     }
 
-    public String getMobile() {
+    public POJO.mobile getMobile() {
         return mobile;
     }
 
-    public void setMobile(String mobile) {
+    public void setMobile(POJO.mobile mobile) {
         this.mobile = mobile;
     }
+
+
 
 
 
