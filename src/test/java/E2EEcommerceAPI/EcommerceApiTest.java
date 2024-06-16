b7c7fc1eb80b3c9fe77a9fdff2472e4932523e47
JIH <mhdnurfaizzy@yahoo.com>
2024-06-16 12:29:02 +0700

    Automate Post calls which has form parameters and attachments using Rest Assured

--- src/test/java/E2EEcommerceAPI/EcommerceApiTest.java
+++ src/test/java/E2EEcommerceAPI/EcommerceApiTest.java
@@ -2,13 +2,18 @@ package E2EEcommerceAPI;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
 public class EcommerceApiTest {
 
     public static void main(String[] args) {
+
+        //Login and get token
 
         RequestSpecification req = new RequestSpecBuilder()
                 .setBaseUri("https://rahulshettyacademy.com")
@@ -27,6 +32,25 @@ public class EcommerceApiTest {
                         .extract().response().as(LoginResponse.class);
 
         System.out.println(loginResponse.getToken());
+        String token = loginResponse.getToken();
         System.out.println(loginResponse.getUserId());
+        String userId = loginResponse.getUserId();
+
+        //Add Product
+        RequestSpecification addProductBaseReq = new RequestSpecBuilder()
+                .setBaseUri("https://rahulshettyacademy.com")
+                .addHeader("authorization", token).build();
+
+        RequestSpecification reqAddProduct = given().log().all().spec(addProductBaseReq).param("productName", "Laptop")
+                .param("productAddedBy", userId).param("productCategory", "fashion")
+                .param("productSubCategory", "shirts").param("productPrice", "11500")
+                .param("productDescription", "Lenova")
+                .param("productFor", "men") .multiPart("productImage", new File("C:\\Users\\HOME\\IdeaProjects\\RestAssuredDemoProject\\Github.png"));
+
+        String addProductResponse = reqAddProduct.when().post("/api/ecom/product/add-product")
+                .then().log().all().extract().response().asString();
+
+        JsonPath js = new JsonPath(addProductResponse);
+        String productId = js.get("productId");
     }
 }
